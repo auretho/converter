9ea67ce248e49f7b3bc62087ce542cc27545a45e
auretho <aurelie.thouzeau@gmail.com>
2021-02-02 13:03:48 +0100

    html css OK

---
+++ src/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+
+const Footer = () => (
+    <div className="footer">
+        <div className="footer-result">1.09</div>
+        <div className="footer-currency">US Dollar</div>
+    </div>
+)
+
+export default Footer;
---
+++ src/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+
+const Header = () => (
+    <div className="header">
+        <h1 className="header-title">Converter</h1>
+        <h2 className="header-subtitle">1 euro</h2>
+    </div>
+)
+
+export default Header;
